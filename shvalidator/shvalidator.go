62387955b2115bfae9718feb3e7b0737a23dc7ed
Sehyoung <kimsh6572@gmail.com>
2022-03-31 15:13:27 +0900

    [patch][shvalidator] update passwaord validator function

--- shvalidator/shvalidator.go
+++ shvalidator/shvalidator.go
@@ -2,6 +2,7 @@ package shvalidator
 
 import (
 	"fmt"
+	"regexp"
 	"strconv"
 	"strings"
 
@@ -22,13 +23,54 @@ var (
 	prefixPhoneNumber = [...]string{"010", "011", "016", "018", "019"}
 )
 
-// TODO
 func IsValidPassword(fl validator.FieldLevel) bool {
 	pw := fl.Field().String()
+
+	// repeating same characters <3
+	cnt := 0
+	var old_v rune
+	for _, v := range pw {
+		if old_v != v {
+			cnt = 0
+			old_v = v
+			continue
+		}
+		cnt++
+		if cnt >= 2 {
+			return false
+		}
+	}
+	// The regexp package uses re2syntax. It doesn't support backreference to keep linear time.
+	// match, _ := regexp.MatchString(`(.)\1\1+`, pw)
+	// if match {
+	// 	fmt.Println("same character")
+	// 	return false
+	// }
+
+	// 8~20
 	fmt.Println(len(pw))
 	if len(pw) < 8 || len(pw) > 20 {
 		return false
 	}
+
+	// alphabet|number|special combination >= 2
+	// This includes all characters àb, 😀 ...
+	// consider changing to below regex.
+	// ~`!@#$%^&*()-_+={}[]|\/:;"'<>,.?₩
+	cnt = 0
+	if match, _ := regexp.MatchString(`[^A-Za-z0-9]`, pw); match {
+		cnt++
+	}
+	if match, _ := regexp.MatchString(`[A-Za-z]`, pw); match {
+		cnt++
+	}
+	if match, _ := regexp.MatchString(`[0-9]`, pw); match {
+		cnt++
+	}
+	if cnt < 2 {
+		return false
+	}
+
 	return true
 }
 
